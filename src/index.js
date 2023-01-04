3d743492628d8b4924325b5168e412e15b3c5c00
TerroR75 <kamilsulimierski@gmail.com>
2023-01-04 21:18:49 +0100

    adding and deleting books from firebase and localstorage

--- src/index.js
+++ src/index.js
@@ -10,14 +10,16 @@ import {
   onSnapshot,
   setDoc,
   updateDoc,
+  deleteDoc,
   doc,
   serverTimestamp,
+  getDoc,
+  getDocs,
 } from 'firebase/firestore';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { getPerformance } from 'firebase/performance';
-import { getDoc } from 'firebase/firestore/lite';
-
+import { v4 as uuidv4 } from 'uuid';
 // === Selectors === //
 const body = document.querySelector('body');
 const sidebar = body.querySelector('.sidebar');
@@ -29,6 +31,8 @@ const modeText = body.querySelector('.mode-text');
 const bookDisplay = body.querySelector('.book-display');
 const bookFunctions = body.querySelector('.book-functions');
 const authBtn = body.querySelector('.auth-btn');
+const signOutButtonElement = body.querySelector('#LOGOUT_BTN');
+const signInButtonElement = body.querySelector('#LOGIN_BTN');
 
 // == Modal related == //
 const btnAddBook = body.querySelector('.btn-add-book');
@@ -43,6 +47,8 @@ const inputTitle = body.querySelector('#title'),
   inputPagesCount = body.querySelector('#pagesCount'),
   inputDate = body.querySelector('#publishmentDate');
 
+let myLibrary = [];
+let currentUser;
 // FIREBASE
 
 const firebaseApp = initializeApp({
@@ -56,13 +62,33 @@ const firebaseApp = initializeApp({
 });
 const db = getFirestore(firebaseApp);
 
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  onAuthStateChanged(getAuth(), authStateObserver);
+}
+
 async function signIn() {
   // Sign in Firebase using popup auth and Google as the identity provider.
   var provider = new GoogleAuthProvider();
   await signInWithPopup(getAuth(), provider);
 }
 
-authBtn.addEventListener('click', signIn);
+async function signOutUser() {
+  myLibrary = [];
+  deleteDataFromLocalStorage('all');
+  // Sign out of Firebase
+  await signOut(getAuth());
+  refreshDisplay();
+  console.log(myLibrary);
+}
+
+function isUserSignedIn() {
+  // TODO 6: Return true if a user is signed-in.
+  return !!getAuth().currentUser;
+}
+
+signInButtonElement.addEventListener('click', signIn);
+signOutButtonElement.addEventListener('click', signOutUser);
 
 // Theme preference
 const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -96,29 +122,38 @@ btnModalClose.addEventListener('click', () => {
 
 btnSubmit.addEventListener('click', addBookToLibrary);
 
+// TODO: Implement searching query
 searchInput.addEventListener('input', () => {
   console.log('xd');
 });
 
-let myLibrary = [];
-// SAVE & LOAD (local storage))
-// loadData();
-
-function Book() {
-  this.title = inputTitle.value;
-  this.author = inputAuthor.value;
-  this.pages = inputPagesCount.value;
-  this.publishDate = inputDate.value;
-  this.isRead = false;
+function Book(book) {
+  if (book == null) {
+    this.title = inputTitle.value;
+    this.author = inputAuthor.value;
+    this.pages = inputPagesCount.value;
+    this.publishDate = inputDate.value;
+    this.isRead = false;
+    this.uid = uuidv4();
+  } else {
+    this.title = book.title;
+    this.author = book.author;
+    this.pages = book.pages;
+    this.publishDate = book.publishDate;
+    this.isRead = book.isRead;
+    this.uid = book.uid;
+  }
 }
 
-function addBookToLibrary() {
+async function addBookToLibrary() {
   modalBg.classList.remove('modal-active');
   modalForm.classList.remove('form-active');
 
-  newBook = new Book();
+  const newBook = new Book();
   myLibrary.push(newBook);
-  // saveData()
+
+  await saveDataToFirebase(newBook);
+  saveToLocalStorage('library', myLibrary);
   clearModalForm();
   refreshDisplay();
 }
@@ -210,7 +245,9 @@ function addBookFunctions() {
     btn.addEventListener('click', () => {
       let userInput = confirm('Are you sure?');
       if (userInput) {
+        removeDocFromFirebase(currentUser.uid, myLibrary[parseInt(btn.dataset.id)].uid);
         myLibrary.splice(parseInt(btn.dataset.id), 1);
+        saveToLocalStorage('library', myLibrary);
         // saveData();
         refreshDisplay();
       }
@@ -243,13 +280,130 @@ function updateStyles() {
   }
 }
 
-function saveData() {
-  let data = JSON.stringify(myLibrary);
-  window.localStorage.setItem('data', data);
+async function saveDataToFirebase(newBook) {
+  try {
+    await setDoc(doc(getFirestore(), `${currentUser.uid}`, `${newBook.uid}`), {
+      title: newBook.title,
+      author: newBook.author,
+      pages: newBook.pages,
+      publishDate: newBook.publishDate,
+      isRead: newBook.isRead,
+      uid: newBook.uid,
+      name: getUserName(),
+    });
+  } catch (error) {
+    console.error('Error writing data to Firebase Database', error);
+  }
 }
 
-function loadData() {
-  let data = window.localStorage.getItem('data');
-  myLibrary = JSON.parse(data);
+async function loadDataFromFirebase() {
+  console.log('Loading data...');
+
+  const querySnapshot = await getDocs(collection(db, currentUser.uid));
+  querySnapshot.forEach((doc) => {
+    const retrievedBook = doc.data();
+    const book = new Book(retrievedBook);
+    myLibrary.push(book);
+  });
+  console.log('Retrieved data from ' + '%cfirebase', 'color: green');
+
+  saveToLocalStorage('library', myLibrary);
+
   refreshDisplay();
 }
+
+async function removeDocFromFirebase(currentUserID, bookID) {
+  console.log('Removing book from firebase...');
+  console.log(`User id: ${currentUserID}`);
+  console.log(`Book id: ${bookID}`);
+  await deleteDoc(doc(getFirestore(), currentUserID, bookID));
+}
+
+function getProfilePicUrl() {
+  // TODO 4: Return the user's profile pic URL.
+  return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
+}
+
+function getUserName() {
+  // TODO 5: Return the user's display name.
+  return getAuth().currentUser.displayName;
+}
+
+// LOCAL STORAGE MODIFICATION FUNCTIONS
+function saveToLocalStorage(key, data) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem(key, dataJSON);
+}
+
+function retrieveDataFromLocalStorage(key) {
+  const retrievedData = JSON.parse(localStorage.getItem(key));
+  console.log(retrievedData);
+  console.log('Retrieved data from ' + '%clocal storage', 'color: green');
+  return retrievedData;
+}
+
+function deleteDataFromLocalStorage(key) {
+  console.log(`localStorage BEFORE: ${localStorage}`);
+  if (key === 'all') {
+    console.log('...Removing ' + '%cwhole' + 'localStorage...', 'color: red');
+    localStorage.clear();
+  } else {
+    console.log('...Removing ' + `%ckey: ${key} ` + 'in localStorage...', 'color: red');
+    localStorage.removeItem(key);
+  }
+  console.log(`localStorage AFTER: ${localStorage}`);
+}
+
+async function authStateObserver(user) {
+  if (user) {
+    console.log('LOGGED IN');
+    currentUser = user;
+
+    // Check if myLibrary variable is empty, if so, check if local storage contains cached data to reduce
+    // firebase retrieving stress
+    if (myLibrary.length === 0) {
+      if (myLibrary.length <= 0) {
+        if (localStorage.getItem('library')) {
+          myLibrary = retrieveDataFromLocalStorage('library');
+          refreshDisplay();
+        } else {
+          await loadDataFromFirebase();
+        }
+      }
+    }
+
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    // var profilePicUrl = getProfilePicUrl();
+    // var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    // userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    // userNameElement.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    // userNameElement.removeAttribute('hidden');
+    // userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+
+    // We save the Firebase Messaging Device token and enable notifications.
+    // saveMessagingDeviceToken();
+  } else {
+    console.log('LOGGED OUT!');
+    currentUser = null;
+    console.log(currentUser);
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    // userNameElement.setAttribute('hidden', 'true');
+    // userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+initFirebaseAuth();
